690d40daa9f43ea58302a36fe6da480845361936
Michael Attoh <47924435+rodmike751@users.noreply.github.com>
2022-11-27 00:53:25 +0000

    first commit

--- setup/settings.py
+++ setup/settings.py
@@ -30,13 +30,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
-    'django_extensions',
 ]
 
-GRAPH_MODELS ={
-'all_applications': True,
-'graph_models': True,
-}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -125,7 +121,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = os.path.join(BASE_DIR, "/media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
